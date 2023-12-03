67d580773fae806cd839b23dba11b0b83854e70b
yakov-yefet <yakov.yefet@gmail.com>
2023-12-03 13:20:43 +0200

    last style

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-
 import streamlit as st
 import yaakov_weather_api as yw
 import pandas as pd
 
 # Title for your app
 st.title("BIU DS16 - Python Project 1 - Weather Forecast")
+st.header('Local Time & Temp')
 
 # local time
-# ist_time = yw.get_date_time()
-# st.write(f"Israel time: {ist_time}")
 st.subheader(yw.preprint_date_time())
-st.code(yw.preprint_temperature_of_city())
+local_temp_text, local_temp_icon = yw.preprint_temperature_of_city()
 
+# set text and icon on the same line
+col1, col2 = st.columns([1, 7])
+with col1:
+    st.image(local_temp_icon, width=50)
+with col2:
+    st.code(local_temp_text)
+
+st.markdown("---")
+st.header('Remote Time & Temp')
 # Dictionary with key-value pairs
 options_dict = {
     "Asia/Jerusalem": "Jerusalem",
@@ -28,7 +35,13 @@ selected_key = yw.get_key(selected_value, options_dict)
 
 # print to screen
 st.subheader(yw.preprint_date_time(selected_key, selected_value))
-st.code(yw.preprint_temperature_of_city(selected_value))
+selected_temp_text, selected_temp_icon = yw.preprint_temperature_of_city(selected_value)
+# set text and icon on the same line
+col3, col4 = st.columns([1, 7])
+with col3:
+    st.image(selected_temp_icon, width=50)
+with col4:
+    st.code(selected_temp_text)
 
 # table
 on = st.toggle(f"Show weather details of ***{selected_value}***")
@@ -36,9 +49,3 @@ on = st.toggle(f"Show weather details of ***{selected_value}***")
 if on:
     df = pd.DataFrame(yw.preprint_weather_of_city(selected_value))
     df
-
-
-
-
-
-
--- yaakov_weather_api.py
+++ yaakov_weather_api.py
@@ -1,54 +1,65 @@
 import streamlit as st
 
-def get_weather_of_city(city_name = "Jerusalem"):
-  import json
-  import requests
-  api_key = 'f55667cb75dd795d3f2454dabce5a616'
-  base_url = "http://api.openweathermap.org/data/2.5/weather?"
-  url = f"{base_url}q={city_name}&appid={api_key}&units=metric"
-  response = requests.get(url)
-  # print(response)
-  weather_json= response.json()
-  # print(weather_json)
-  formatted_json = json.dumps(weather_json, indent=2)
-  # print(formatted_json)
-  return weather_json
 
-def preprint_temperature_of_city(city_name = "Jerusalem"):
-  weather_json = get_weather_of_city(city_name)
-  temperature = weather_json['main']['temp']
-  return f"Temperature in {city_name}: {temperature} Celsius"
-  # st.write(f"Temperature in {city_name}: {temperature} Celsius")
-  # print(f"Temperature in {city_name}: {temperature} Celsius")
+def get_weather_of_city(city_name="Jerusalem"):
+    import json
+    import requests
+    api_key = 'f55667cb75dd795d3f2454dabce5a616'
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    url = f"{base_url}q={city_name}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    # print(response)
+    weather_json = response.json()
+    # print(weather_json)
+    formatted_json = json.dumps(weather_json, indent=2)
+    # print(formatted_json)
+    return weather_json
 
-def preprint_weather_of_city(city_name = "Jerusalem"):
-  weather_json = get_weather_of_city(city_name)
+def preprint_temperature_of_city(city_name="Jerusalem"):
+    weather_json = get_weather_of_city(city_name)
+    temperature = weather_json['main']['temp']
+    # Extract icon code
+    icon_code = weather_json["weather"][0]["icon"]
+    # Construct icon URL
+    text = f"Temperature in {city_name}: {temperature} Celsius"
+    icon_url = f"http://openweathermap.org/img/wn/{icon_code}.png"
+    icon = icon_url
+    return text, icon
 
-  weather_data = {
-    'Temperature': [weather_json['main']['temp']],
-    'Feels like': [weather_json['main']['feels_like']],
-    'Temp min': [weather_json['main']['temp_min']],
-    'Temp max': [weather_json['main']['temp_max']],
-    'Humidity': [weather_json['main']['humidity']],
-    'Wind speed': [weather_json['wind']['speed']]
-  }
-  return weather_data
+
+# return f"Temperature in {city_name}: {temperature} Celsius"
+
+
+def preprint_weather_of_city(city_name="Jerusalem"):
+    weather_json = get_weather_of_city(city_name)
+
+    weather_data = {
+        'Temperature': [weather_json['main']['temp']],
+        'Feels like': [weather_json['main']['feels_like']],
+        'Temp min': [weather_json['main']['temp_min']],
+        'Temp max': [weather_json['main']['temp_max']],
+        'Humidity': [weather_json['main']['humidity']],
+        'Wind speed': [weather_json['wind']['speed']]
+    }
+    return weather_data
+
 
 def get_date_time(location_timezone_str='Asia/Jerusalem'):
-  import datetime as dt
-  import pytz
-  import tzlocal
-  now = dt.datetime.now()
-  utc_tz = pytz.timezone('UTC')
-  location_tz = pytz.timezone(location_timezone_str)
-  utc_time = utc_tz.localize(now)
-  location_time = utc_time.astimezone(location_tz)
-  return location_time.strftime('%d.%m.%Y  -  %H:%M')
+    import datetime as dt
+    import pytz
+    import tzlocal
+    now = dt.datetime.now()
+    utc_tz = pytz.timezone('UTC')
+    location_tz = pytz.timezone(location_timezone_str)
+    utc_time = utc_tz.localize(now)
+    location_time = utc_time.astimezone(location_tz)
+    return location_time.strftime('%d.%m.%Y  -  %H:%M')
 
-def preprint_date_time(location_timezone_str = 'Asia/Jerusalem', location_str = 'Jerusalem'):
-  return f"Date and time in ***{location_str}***: {get_date_time(location_timezone_str)}"
-  # st.write(f"Date and time in {location_str}: {get_date_time(location_timezone_str)}")
-  # print(f"Date and time in {location_str}: {get_date_time(location_timezone_str)}")
+
+def preprint_date_time(location_timezone_str='Asia/Jerusalem', location_str='Jerusalem'):
+    return f"Date and time in ***{location_str}***: {get_date_time(location_timezone_str)}"
+    # st.write(f"Date and time in {location_str}: {get_date_time(location_timezone_str)}")
+    # print(f"Date and time in {location_str}: {get_date_time(location_timezone_str)}")
 
 
 # Function to get key by value
@@ -57,7 +68,3 @@ def get_key(val, options_dict):
         if val == value:
             return key
     return "Key not found"
-
-
-
-
